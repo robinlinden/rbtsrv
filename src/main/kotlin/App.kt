b5e1d8b1966b92054cc4b5cbee2bce0fd2ded932
Robin Linden <dev@robinlinden.eu>
2019-12-04 23:06:21 +0100

    Publish received messages on NATS

--- src/main/kotlin/App.kt
+++ src/main/kotlin/App.kt
@@ -1,14 +1,26 @@
 import io.grpc.ServerBuilder
 import io.grpc.stub.StreamObserver
+import io.nats.client.*
 import ltd.evilcorp.rbtsrv.GreeterGrpc
 import ltd.evilcorp.rbtsrv.HelloReply
 import ltd.evilcorp.rbtsrv.HelloRequest
+import java.lang.Exception
 
 class GreeterImpl : GreeterGrpc.GreeterImplBase() {
+    private val natsConnection: Connection = Nats.connect(
+        Options.Builder().errorListener(object : ErrorListener {
+            override fun errorOccurred(conn: Connection?, error: String?) = println("error: $error")
+            override fun exceptionOccurred(conn: Connection?, exp: Exception?) = println("exception: ${exp?.message}")
+            override fun slowConsumerDetected(conn: Connection?, consumer: Consumer?) {}
+        }).build()
+    )
+
     override fun sayHello(r: HelloRequest, responseObserver: StreamObserver<HelloReply>) {
         val reply = HelloReply.newBuilder().setMessage("Hello ${r.name}.").build()
         responseObserver.onNext(reply)
         responseObserver.onCompleted()
+
+        natsConnection.publish("greeting", r.name.toByteArray())
     }
 }
 
