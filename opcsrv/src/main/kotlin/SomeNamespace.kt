009875978770682c09ebe59ff8d872cc3c8f6e99
Robin Linden <dev@robinlinden.eu>
2019-12-08 21:00:42 +0100

    Simplify adding OPC nodes and folders

--- opcsrv/src/main/kotlin/SomeNamespace.kt
+++ opcsrv/src/main/kotlin/SomeNamespace.kt
@@ -1,10 +1,10 @@
-import org.eclipse.milo.opcua.sdk.core.AccessLevel
 import org.eclipse.milo.opcua.sdk.core.Reference
 import org.eclipse.milo.opcua.sdk.server.OpcUaServer
 import org.eclipse.milo.opcua.sdk.server.api.DataItem
 import org.eclipse.milo.opcua.sdk.server.api.ManagedNamespace
 import org.eclipse.milo.opcua.sdk.server.api.MonitoredItem
 import org.eclipse.milo.opcua.sdk.server.nodes.UaFolderNode
+import org.eclipse.milo.opcua.sdk.server.nodes.UaNode
 import org.eclipse.milo.opcua.sdk.server.nodes.UaVariableNode
 import org.eclipse.milo.opcua.sdk.server.util.SubscriptionModel
 import org.eclipse.milo.opcua.stack.core.Identifiers
@@ -12,49 +12,63 @@ import org.eclipse.milo.opcua.stack.core.types.builtin.DataValue
 import org.eclipse.milo.opcua.stack.core.types.builtin.LocalizedText
 import org.eclipse.milo.opcua.stack.core.types.builtin.NodeId
 import org.eclipse.milo.opcua.stack.core.types.builtin.Variant
-import org.eclipse.milo.opcua.stack.core.types.builtin.unsigned.Unsigned
 
-class SomeNamespace(server: OpcUaServer) : ManagedNamespace(server, "evilcorp:rbtsrv") {
+class SomeNamespace(server: OpcUaServer) : ManagedNamespace(server, "urn:evilcorp:rbtsrv") {
     private val subscriptionModel = SubscriptionModel(server, this)
+
+    private fun createFolder(nodePath: String) {
+        val segments = nodePath.split('/')
+        val nodeName = segments.last()
+        val folderNodeId: NodeId = newNodeId(nodePath)
+        val folderNode = UaFolderNode(
+            nodeContext,
+            folderNodeId,
+            newQualifiedName(nodeName),
+            LocalizedText.english(nodeName)
+        )
+
+        addNode(folderNode)
+    }
+
+    private fun createStringNode(nodeId: String): UaVariableNode {
+        val segments = nodeId.split('/')
+        val nodeName = segments.last()
+
+        val node = UaVariableNode.UaVariableNodeBuilder(nodeContext)
+            .setNodeId(newNodeId(nodeId))
+            .setBrowseName(newQualifiedName(nodeName))
+            .setDisplayName(LocalizedText.english(nodeName))
+            .setDataType(Identifiers.String)
+            .setTypeDefinition(Identifiers.BaseDataVariableType)
+            .build()
+
+        addNode(node)
+        return node
+    }
+
+    private fun addNode(node: UaNode) {
+        nodeManager.addNode(node)
+        val segments = node.nodeId.identifier.toString().split('/')
+
+        val parentNode = if (segments.size > 1) {
+            newNodeId(segments.dropLast(1).reduce { acc, s -> "$acc/$s" }).expanded()
+        } else {
+            Identifiers.ObjectsFolder.expanded()
+        }
+
+        node.addReference(Reference(node.nodeId, Identifiers.Organizes, parentNode, false))
+    }
 
     override fun onStartup() {
         super.onStartup()
 
-        val folderNodeId: NodeId = newNodeId("Test")
-
-        val folderNode = UaFolderNode(
-            nodeContext,
-            folderNodeId,
-            newQualifiedName("Test"),
-            LocalizedText.english("Test")
-        )
-
-        nodeManager.addNode(folderNode)
-
-        folderNode.addReference(
-            Reference(
-                folderNode.nodeId,
-                Identifiers.Organizes,
-                Identifiers.ObjectsFolder.expanded(),
-                false
-            )
-        )
-
-        val name = "Name"
-        val variant = Variant("Robotto")
-        val node = UaVariableNode.UaVariableNodeBuilder(nodeContext)
-            .setNodeId(newNodeId("Test/$name"))
-            .setAccessLevel(Unsigned.ubyte(AccessLevel.getMask(AccessLevel.READ_WRITE)))
-            .setUserAccessLevel(Unsigned.ubyte(AccessLevel.getMask(AccessLevel.READ_WRITE)))
-            .setBrowseName(newQualifiedName(name))
-            .setDisplayName(LocalizedText.english(name))
-            .setDataType(Identifiers.String)
-            .setTypeDefinition(Identifiers.BaseDataVariableType)
-            .build().apply {
-                value = DataValue(variant)
-            }
-        nodeManager.addNode(node)
-        folderNode.addOrganizes(node)
+        createFolder("Test")
+        createFolder("Test/Test")
+        createFolder("Test/Test/MoreTest")
+        createFolder("Test/SomeNode")
+        createStringNode("SomeRootNode").value = DataValue(Variant("Root node"))
+        createStringNode("Test/Name").value = DataValue(Variant("Robotto"))
+        createStringNode("Test/Test/MoreTest/Magic").value = DataValue(Variant("beep beep boop"))
     }
 
     override fun onDataItemsDeleted(dataItems: MutableList<DataItem>?) =
