46e356ea24a5db7ce1772376f13d08d3ff97fe0c
Robin Linden <dev@robinlinden.eu>
2022-05-20 17:48:43 +0200

    Update gradle-versions-plugin to allow checking for newer dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,12 +2,12 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.6.10" apply false
-    id("com.github.ben-manes.versions") version "0.27.0"
+    id("com.github.ben-manes.versions") version "0.42.0"
 }
 
 buildscript {
     dependencies {
-        classpath("com.github.ben-manes:gradle-versions-plugin:0.27.0")
+        classpath("com.github.ben-manes:gradle-versions-plugin:0.42.0")
     }
 }
 
