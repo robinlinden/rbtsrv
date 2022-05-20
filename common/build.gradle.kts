1a23f86c7faddf7616d895b86592696dfe8d33b9
Robin Linden <dev@robinlinden.eu>
2022-05-20 18:37:24 +0200

    Update everything except Eclipse Milo

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -6,5 +6,5 @@ version = "0.1.0"
 
 dependencies {
     // NATS
-    api("io.nats:jnats:2.6.6")
+    api("io.nats:jnats:2.15.0")
 }
--- grpcsrv/build.gradle.kts
+++ grpcsrv/build.gradle.kts
@@ -3,7 +3,7 @@ import com.google.protobuf.gradle.*
 plugins {
     kotlin("jvm")
     idea
-    id("com.google.protobuf") version "0.8.11"
+    id("com.google.protobuf") version "0.8.18"
 }
 
 version = "0.1.0"
@@ -12,22 +12,22 @@ dependencies {
     implementation(project(":common"))
 
     // grpc/protobuf
-    implementation("io.grpc:grpc-netty-shaded:1.25.0")
-    implementation("io.grpc:grpc-stub:1.25.0")
-    implementation("io.grpc:grpc-protobuf:1.25.0")
+    implementation("io.grpc:grpc-netty-shaded:1.46.0")
+    implementation("io.grpc:grpc-stub:1.46.0")
+    implementation("io.grpc:grpc-protobuf:1.46.0")
     implementation("javax.annotation:javax.annotation-api:1.3.2")
 
-    testImplementation("org.junit.jupiter:junit-jupiter-engine:5.5.2")
-    testImplementation("io.mockk:mockk:1.9.3")
+    testImplementation("org.junit.jupiter:junit-jupiter-engine:5.8.2")
+    testImplementation("io.mockk:mockk:1.12.4")
 }
 
 protobuf {
     protoc {
-        artifact = "com.google.protobuf:protoc:3.11.1"
+        artifact = "com.google.protobuf:protoc:3.20.1"
     }
     plugins {
         id("grpc") {
-            artifact = "io.grpc:protoc-gen-grpc-java:1.25.0"
+            artifact = "io.grpc:protoc-gen-grpc-java:1.46.0"
         }
     }
     generateProtoTasks {
